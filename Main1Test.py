7417d5b32e921905821b7883bde1fe3fff8570be
justinthompson <jdthompson88@hotmail.com>
2019-10-27 15:49:40 -0400

    Made some changes.

--- Main1Test.py
+++ Main1Test.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def main(msg):
+    # comments in python are made with a hash tag NOT
+    # the double forward slash That is for everyone else, as far as I am aware.
     print(msg)
 
 main("Hello People")
